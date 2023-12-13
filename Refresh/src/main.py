0fdd89f4679cd67f7916d9995f688a4a9ecb2def
Victor Kaklamanis <vic.kaklamanis@gmail.com>
2023-12-13 23:53:47 +0200

    Added more generalised test functions for an indefinite number of players.

--- Refresh/src/main.py
+++ Refresh/src/main.py
@@ -5,21 +5,33 @@ class CurrentBet:
     def __init__(self, value: float = 1.0):
         self.value = round(value, 2)
 
-
 class Player:
     amount: float
     bet_amount: CurrentBet
 
-    def __init__(self, bet_amount: CurrentBet, amount: float = 0.0):
+    def __init__(self, amount: float = 0.0):
         self.amount = round(amount, 2)
+        # self.bet_amount = 0.0
+
+    def bet_an_amount(self, bet_amount: CurrentBet):
         self.bet_amount = bet_amount
+        self.amount -= self.bet_amount.value
+
+
+def all_players_bet(players: list, bet_amount: CurrentBet):
+    for player in players:
+        player.bet_an_amount(bet_amount)
 
 
 def run():
     current_bet = CurrentBet(1.25)
 
-    p1 = Player(amount=10.34, bet_amount=current_bet)
-    p2 = Player(amount=8.95, bet_amount=current_bet)
+    p1 = Player(amount=10.34)
+    p2 = Player(amount=8.95)
+
+    list_of_players = [p1, p2]
+
+    # all_players_bet(list_of_players, current_bet)
 
     return p1, p2
 
--- Refresh/test/test_main.py
+++ Refresh/test/test_main.py
@@ -2,22 +2,31 @@ from Refresh.src import main
 import pytest
 
 """I don't remember how to setup the pytest.parametrize function. 
-However, even though this appears to be a docstring, I am actually 
-testing my new keyboard."""
+However, even though this initially appears to be a docstring, I 
+am actually testing my new keyboard."""
 
-def setup():
-    return list(main.run())
+"""def setup():
+    return list(main.run())"""
 
-def test_player_class():
-    result = setup()
-    assert isinstance(result, main.Player)
+result = main.run()
+current_bet = main.CurrentBet(1.25)
 
-def test_player_has_amount():
-    result = setup()
-    assert result[0].amount == 0.0
+def test_player_class(players=result):
+    players_are_true = [isinstance(player, main.Player) for player in players]
+    assert bool(set(players_are_true))
 
-def test_bet_amount_is_equal_to_all_players():
-    result = setup()
-    assert len(set([player.bet_amount for player in result])) == 1
+def test_player_has_amount(players=result):
+    assert isinstance(players[0].amount, float)
 
+def test_bet_amount_is_equal_to_all_players(players=result):
+    assert len(set([player.bet_amount for player in players])) == 1
 
+def test_all_players_bet_an_amount(players=result, bet=current_bet):
+    initial_amounts = [player.amount for player in players]
+    main.all_players_bet(players, bet)
+    new_amounts = [player.amount for player in players]
+
+    pair_of_old_new_values = list(zip(initial_amounts, new_amounts))
+    assert float(sum(set([old - new for old, new in pair_of_old_new_values]))) == current_bet.value
+
+    
